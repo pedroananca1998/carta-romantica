732edd735816bb89dc961bdf38968444e1963612
pedroananca1998 <120835423+pedroananca1998@users.noreply.github.com>
2025-12-24 18:10:12 -0500

    Add files via upload

---
+++ latido.js
@@ -0,0 +1,8 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const carta = document.querySelector(".carta");
+
+    setTimeout(() => {
+        carta.classList.remove("dormida");
+        carta.classList.add("despierta");
+    }, 600);
+});
